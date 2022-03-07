488830a20b888ef426c8f00b49c6b3f1b4a3a8ca
roodligo <ti.rodrigo@outlook.com>
2022-03-07 19:20:17 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# praticando_web
---
+++ php/formulario.php
@@ -0,0 +1,13 @@
+<?php
+    $nome = $_GET["nome"];
+    $email = $_GET["email"];
+    $data_nascimento = $_GET["data_nascimento"];
+    $sexo = $_GET["sexo"];
+    $estado = $_GET["estado"];
+    $senha = $_GET["senha"];
+    $confere_senha = $_GET["confere_senha"];
+    $aceita_news = $_GET["aceita_news"];
+    $observacoes = $_GET["observacoes"];
+    
+    echo "Nome: ".$nome;
+?>
