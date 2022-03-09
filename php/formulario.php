a9a9f01927c9b0dedb12fb44fc8d0fccb8bedf13
roodligo <ti.rodrigo@outlook.com>
2022-03-08 22:36:37 -0300

    conectando

--- php/formulario.php
+++ php/formulario.php
@@ -1,18 +1,24 @@
 <?php
-    $nome = $_GET["nome"];
-    $email = $_GET["email"];
-    $data_nascimento = $_GET["data_nascimento"];
-    $sexo = $_GET["sexo"];
-    $estado = $_GET["estado"];
-    $senha = $_GET["senha"];
-    $confere_senha = $_GET["confere_senha"];
-    $aceita_news = $_GET["aceite_news"];
-    $observacoes = $_GET["observacoes"];
-    echo "Olá ".$nome;
-    if ($senha == $confere_senha){
-        echo " você foi Cadastrado com Sucesso!";
-    }
-    else{
-        echo " a Senha digitada é Invalida";
-    }
+    $nome = $_POST["nome"];
+    $email = $_POST["email"];
+    $data_nascimento = $_POST["data_nascimento"];
+    $sexo = $_POST["sexo"];
+    $estado = $_POST["estado"];
+    $senha = $_POST["senha"];
+    $confere_senha = $_POST["confere_senha"];
+    $aceita_news = 0;
+    $aceita_news = $_POST["aceite_news"];
+    $observacoes = $_POST["observacoes"];
+        if ($senha == $confere_senha)
+        {
+            $conexao = mysqli_connect('localhost','root','d4rks1d3','cad') or die("Erro ao Conectar");
+            $string = "INSERT INTO cad_cliente VALUES";
+            $string .="('$nome','$email','$data_nascimento','$sexo','$estado','$senha','$aceita_news','$observacoes')";
+            mysqli_query($conexao,$string) or die("Erro ao Cadastrar");
+            echo "Cadastro Efetuado com Sucesso!";
+        }else 
+        {
+            echo "As Senhas Digitadas nao Batem";
+        }
+        
 ?>
