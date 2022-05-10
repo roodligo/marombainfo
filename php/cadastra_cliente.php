89d662a42eedcc27e7b506c60b06772773e45852
roodligo <ti.rodrigo@outlook.com>
2022-05-09 21:57:03 -0300

    criado validar senha

--- php/cadastra_cliente.php
+++ php/cadastra_cliente.php
@@ -1,23 +1,24 @@
 <?php
 include_once "conexao.php";
 
-    $nome = $_POST["nome"];
-    $email = $_POST["email"];
-    $senha = $_POST["senha"];
-    $sexo = $_POST["sexo"];
-    $cep = $_POST["cep"];
-    $rua = $_POST["rua"];
-    $numero = $_POST["numero_casa"];
-    $complemento = $_POST["complemento"];
-    $bairro = $_POST["bairro"];
-    $cidade = $_POST["cidade"];
-    $estado = $_POST["uf"];
-    $aceita_news = 0;
+$nome = $_POST["nome"];
+$email = $_POST["email"];
+$senha = $_POST["senha"];
+$sexo = $_POST["sexo"];
+$cep = $_POST["cep"];
+$rua = $_POST["rua"];
+$numero = $_POST["numero_casa"];
+$complemento = $_POST["complemento"];
+$bairro = $_POST["bairro"];
+$cidade = $_POST["cidade"];
+$estado = $_POST["uf"];
+$aceita_news = 0;
 
-            $string = "INSERT INTO cad_cliente VALUES";
-            $string .="(NULL,'$nome','$email','$senha','$sexo','$cep','$rua','$numero','$complemento','$bairro','$cidade','$estado')";
-            mysqli_query($conn,$string) or die("Erro ao Cadastrar");
-            echo "Cadastro Efetuado com Sucesso!";
-        
-        
+$string = "INSERT INTO cad_cliente VALUES";
+$string .= "(NULL,'$nome','$email','$senha','$sexo','$cep','$rua','$numero','$complemento','$bairro','$cidade','$estado')";
+mysqli_query($conn, $string) or die("Erro ao Cadastrar");
 ?>
+<script type="text/javascript">
+    alert('Cadastro Efetuado com Sucesso!');
+    window.location.href = "../index.html";
+</script>
