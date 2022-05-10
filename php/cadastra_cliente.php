663f8cf752ee5a12ee60f7af77358c5deda9f337
roodligo <ti.rodrigo@outlook.com>
2022-05-10 16:16:54 -0300

    criado metodo de login

--- php/cadastra_cliente.php
+++ php/cadastra_cliente.php
@@ -3,7 +3,7 @@ include_once "conexao.php";
 
 $nome = $_POST["nome"];
 $email = $_POST["email"];
-$senha = $_POST["senha"];
+$hashsenha = trim(password_hash($_POST['senha'], PASSWORD_DEFAULT));
 $sexo = $_POST["sexo"];
 $cep = $_POST["cep"];
 $rua = $_POST["rua"];
@@ -14,8 +14,10 @@ $cidade = $_POST["cidade"];
 $estado = $_POST["uf"];
 $aceita_news = 0;
 
+
+
 $string = "INSERT INTO cad_cliente VALUES";
-$string .= "(NULL,'$nome','$email','$senha','$sexo','$cep','$rua','$numero','$complemento','$bairro','$cidade','$estado')";
+$string .= "(NULL,'$nome','$email','$hashsenha','$sexo','$cep','$rua','$numero','$complemento','$bairro','$cidade','$estado')";
 mysqli_query($conn, $string) or die("Erro ao Cadastrar");
 ?>
 <script type="text/javascript">
---
+++ php/login_cliente.php
@@ -0,0 +1,27 @@
+<?php
+include_once "conexao.php";
+
+
+$email = $_POST["email"];
+$senha = $_POST["senha"];
+$sql = "SELECT * FROM cad_cliente WHERE email = '$email'";
+$result = mysqli_query($conn, $sql);
+$num = mysqli_num_rows($result);
+if ($num == 1) {
+    while ($row = mysqli_fetch_assoc($result)) {
+        if (password_verify($senha, $row['senha'])) {
+?>
+            <script type="text/javascript">
+                alert('Login Efetuado com Sucesso!');
+                window.location.href = "../index.html";
+            </script><?php
+                    } else {
+                        ?>
+            <script type="text/javascript">
+                alert('E-mail ou Senha Inválida!');
+                window.location.href = "../html/login.html";
+            </script><?php
+                    }
+                }
+            }
+                        ?>
