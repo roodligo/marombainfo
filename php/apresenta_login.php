f1741765a85b08b442985dfa5c7a8b9447f717aa
roodligo <ti.rodrigo@outlook.com>
2022-06-03 21:13:23 -0300

    borda no botao login

--- php/apresenta_login.php
+++ php/apresenta_login.php
@@ -8,14 +8,14 @@ if (isset($_SESSION['email'])) {
     $email = ($_SESSION['email']);
     $result = $conn->query("SELECT * FROM cad_cliente WHERE email = '$email'");
     while ($obj = $result->fetch_object()) { 
-        echo '<div class="container"><h5 style="margin: auto;">' . $obj->nome . '  </h5> <form action="../php/logoff.php"><input type="submit" class="btn btn-danger" value="Sair" /></form> </div>';
+        echo '<div class="container"><h5 style="margin: auto;">' . $obj->nome . '  </h5> <form action="../php/logoff.php"><input type="submit" class="btn btn-danger" style="border: 1px solid black" value="Sair" /></form> </div>';
     }
 ?>
     
 <?php
 } else {
 ?>
-    <form action="/html/login-cadastro.html"> <input type="submit" class="btn btn-danger" value="Faça Login ou Cadastre-se" />
+    <form action="/html/login-cadastro.html"> <input type="submit" class="btn btn-danger" style="border: 1px solid black;" value="Faça Login ou Cadastre-se" />
     </form> <?php
         }
             ?>
